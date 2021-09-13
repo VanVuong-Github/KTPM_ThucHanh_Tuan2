29620967d17fbe963f1e43ff6227a30d3c8ac056
VanVuong <lai.vanvuong2866@gmail.com>
2021-09-13 11:20:21 +0700

    first commit

---
+++ src/main/java/com/exercise/tuan2/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.exercise.tuan2;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Tuan2Application.class);
+	}
+
+}
---
+++ src/test/java/com/exercise/tuan2/Tuan2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.tuan2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tuan2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
